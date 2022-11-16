5bd43e1760d3361e08de18803980cad6706f9cfb
geier.zsolt <zsoltgeier@stud.uni-obuda.hu>
2022-11-16 17:07:45 +0100

    Dealership class created

---
+++ SJIDON_HFT_2022231.Models/Dealership.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SJIDON_HFT_2022231.Models
+{
+    [Table("Dealerships")]
+    public class Dealership
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public int Employees { get; set; }
+    }
+}
