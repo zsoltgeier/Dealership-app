56fd078312bd33b86481fc71dd48e64693360d04
geier.zsolt <zsoltgeier@stud.uni-obuda.hu>
2022-11-19 17:11:57 +0100

    DealershipLogic interface added

---
+++ SJIDON_HFT_2022231.Logic/IDealershipLogic.cs
@@ -0,0 +1,20 @@
+﻿using SJIDON_HFT_2022231.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SJIDON_HFT_2022231.Logic
+{
+    internal interface IDealershipLogic
+    {
+        //CRUD
+        void Create(Dealership obj);
+        Dealership Read(int id);
+        IQueryable<Dealership> ReadAll();
+        void Update(Dealership obj);
+        void Delete(int id);
+
+    }
+}
