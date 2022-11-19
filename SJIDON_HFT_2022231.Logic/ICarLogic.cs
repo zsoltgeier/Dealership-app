bba02d8030d92009a134fe7fe04044e16c6cb57c
geier.zsolt <zsoltgeier@stud.uni-obuda.hu>
2022-11-19 17:15:37 +0100

    CarLogic interface created

---
+++ SJIDON_HFT_2022231.Logic/ICarLogic.cs
@@ -0,0 +1,19 @@
+﻿using SJIDON_HFT_2022231.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SJIDON_HFT_2022231.Logic
+{
+    internal interface ICarLogic
+    {
+        //CRUD
+        void Create(Car obj);
+        Car Read(int id);
+        IQueryable<Car> ReadAll();
+        void Update(Car obj);
+        void Delete(int id);
+    }
+}
