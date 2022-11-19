3df8e27a366d18006d4a3d7eef6016497f5e82bd
geier.zsolt <zsoltgeier@stud.uni-obuda.hu>
2022-11-19 17:14:38 +0100

    BrandLogic interface created

---
+++ SJIDON_HFT_2022231.Logic/IBrandLogic.cs
@@ -0,0 +1,19 @@
+﻿using SJIDON_HFT_2022231.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SJIDON_HFT_2022231.Logic
+{
+    internal interface IBrandLogic
+    {
+        //CRUD
+        void Create(Brand obj);
+        Brand Read(int id);
+        IQueryable<Brand> ReadAll();
+        void Update(Brand obj);
+        void Delete(int id);
+    }
+}
