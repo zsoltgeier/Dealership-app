0c4ce9278cb968fc8b48ead595f32a6ea7f9adb3
geier.zsolt <zsoltgeier@stud.uni-obuda.hu>
2022-11-20 11:58:21 +0100

    Added non-CRUD methods to CarLogic class

--- SJIDON_HFT_2022231.Logic/CarLogic.cs
+++ SJIDON_HFT_2022231.Logic/CarLogic.cs
@@ -10,6 +10,8 @@ namespace SJIDON_HFT_2022231.Logic
 {
     public class CarLogic : ICarLogic
     {
+        IRepository<Dealership> dealershipRepo;
+        IRepository<Brand> brandRepo;
         IRepository<Car> carRepo;
 
         public CarLogic(IRepository<Car> carRepo)
@@ -51,6 +53,39 @@ namespace SJIDON_HFT_2022231.Logic
         public void Update(Car obj)
         {
             carRepo.Update(obj);
+        }
+
+        public IEnumerable<Car> GetCarFromJapaneseDealership()
+        {
+            var q = from cars in carRepo.ReadAll()
+                    join brands in brandRepo.ReadAll()
+                    on cars.Brand_Id equals brands.Id
+                    join dealerships in dealershipRepo.ReadAll()
+                    on brands.Dealership_Id equals dealerships.Id
+                    where dealerships.Name == "Japanese car dealership"
+                    select cars;
+            return q;
+        }
+
+        public IEnumerable<Car> GetCarWhereMoreThan18Employees()
+        {
+            var q = from cars in carRepo.ReadAll()
+                    join brands in brandRepo.ReadAll()
+                    on cars.Brand_Id equals brands.Id
+                    join dealerships in dealershipRepo.ReadAll()
+                    on brands.Dealership_Id equals dealerships.Id
+                    where dealerships.Employees > 18
+                    select cars;
+            return q;
+        }
+        public IEnumerable<Car> GetCarWhereBrandOwnerIsBMWGroup()
+        {
+            var q = from cars in carRepo.ReadAll()
+                    join brands in brandRepo.ReadAll()
+                    on cars.Brand_Id equals brands.Id
+                    where brands.Owner == "BMW Group"
+                    select cars;
+            return q;
         }
     }
 }
