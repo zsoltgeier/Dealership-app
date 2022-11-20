35dc3aff0b7de58277f9d79e9edc6ef071b8edfc
geier.zsolt <zsoltgeier@stud.uni-obuda.hu>
2022-11-20 20:04:12 +0100

    Startup class modified

--- SJIDON_HFT_2022231.Endpoint/Startup.cs
+++ SJIDON_HFT_2022231.Endpoint/Startup.cs
@@ -1,8 +1,12 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using SJIDON_HFT_2022231.Logic;
+using SJIDON_HFT_2022231.Models;
+using SJIDON_HFT_2022231.Repository;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -16,6 +20,17 @@ namespace SJIDON_HFT_2022231.Endpoint
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddTransient<ICarLogic, CarLogic>();
+            services.AddTransient<IBrandLogic, BrandLogic>();
+            services.AddTransient<IDealershipLogic, DealershipLogic>();
+
+            services.AddTransient<IRepository<Car>, CarRepository>();
+            services.AddTransient<IRepository<Brand>, BrandRepository>();
+            services.AddTransient<IRepository<Dealership>, DealershipRepository>();
+
+            services.AddSingleton<DbContext, CarDbContext>();
+
+            services.AddControllers();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -30,10 +45,7 @@ namespace SJIDON_HFT_2022231.Endpoint
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapControllers();
             });
         }
     }
